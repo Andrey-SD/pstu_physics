b5a2effdce28aa9932ae88d9540bc1c02676a1bc
Andrey-SD <andrey.sd.post@gmail.com>
2025-11-19 14:27:59 +0200

    Added ЛР76

---
+++ ЛР76/js/script.js
@@ -0,0 +1,125 @@
+let isOn = false;
+let isModalShown = false;
+let previousSelectOption = "white";
+const filter = $(".filter-img");
+
+$(".filter").on("change", function () {
+  // if (isOn) {
+  //   alert("Спочатку вимкніть прилад");
+  //   $(this).val(previousSelectOption);
+  // }
+  const value = $(this).val();
+  const index = $(".range").val();
+  previousSelectOption = value;
+  let coeff = 1;
+
+  switch (value) {
+    default:
+    case "white":
+      coeff = 1;
+      break;
+    case "blue":
+      coeff = 1.111;
+      break;
+    case "green":
+      coeff = 1.39;
+      break;
+    case "red":
+      coeff = 1.722;
+      break;
+  }
+
+  if (value === "white") {
+    filter.prop("src", `./img/interference-${index}.jpg`);
+  } else if (value == null) {
+    return;
+  } else {
+    filter.prop("src", `./img/interference-${index}-${value}.png`);
+  }
+  filter.css("transform", "scaleX(" + coeff + ")");
+});
+
+$(".range").on("input", function () {
+  const value = $(this).val();
+  const color = $(".filter").val();
+
+  let coeff = 1;
+
+  switch (color) {
+    default:
+    case "white":
+      coeff = 1;
+      break;
+    case "blue":
+      coeff = 1.111;
+      break;
+    case "green":
+      coeff = 1.39;
+      break;
+    case "red":
+      coeff = 1.722;
+      break;
+      -transp;
+  }
+
+  if (color == "white") {
+    filter.prop("src", `./img/interference-${value}.jpg`);
+  } else if (color == null) {
+    return;
+  } else {
+    filter.prop("src", `./img/interference-${value}-${color}.png`);
+  }
+  filter.css("transform", `scaleX(${coeff})`);
+});
+
+$(".slider-btn, .button.slider").on("click", toggleSlider);
+
+function toggleSlider() {
+  if (isOn) {
+    $(".slider-img").prop("src", "./img/slider-modif-transp.png");
+    filter.css("opacity", 0);
+    $(".button.slider").html("Увімкнути пристрій");
+  } else {
+    $(".slider-img").prop("src", "./img/slider-modif-on-transp.png");
+    filter.css("opacity", 1);
+    $(".button.slider").html("Вимикнути пристрій");
+  }
+  isOn = !isOn;
+}
+
+$(".close-btn, .button-ocular, .ocular-btn").on("click", toggleModal);
+
+function toggleModal() {
+  // $(".scaleRange").val(0);
+  // $(".scale").css("margin-right", "0%");
+
+  // if (!isOn) {
+  //   alert("Спочатку увімкніть прилад");
+  //   return;
+  // }
+  // if ($(".filter").val() == null) {
+  //   alert("Спочатку виберіть фільтр");
+  //   return;
+  // }
+
+  if (isModalShown) {
+    $(".modal").css("display", "none");
+  } else {
+    $(".modal").css("display", "flex");
+  }
+  isModalShown = !isModalShown;
+}
+
+$(".scaleRange").on("input", function (e) {
+  const percentage = parseInt($(this).val());
+
+  $(".scale").css("margin-left", percentage + "%");
+});
+
+$(".modal").on("click", function (e) {
+  if (e.target !== e.currentTarget) {
+    return;
+  }
+
+  toggleModal();
+});
